a9a28e0a04dd3608aeab0595ba19f14d88fca1f0
Elly Pham <elly.pham@shopify.com>
2023-07-04 21:48:21 -0400

    Initial REST API for tricks

--- backend/controllers/trickController.js
+++ backend/controllers/trickController.js
@@ -1,45 +1,69 @@
 const asyncHandler = require('express-async-handler')
 
+const Trick = require('../models/trickModel')
+
 // @desc Get tricks
 // @route GET /api/tricks
 // @access Private
 
-const getTricks = aysncHandler(async (req, res) => {
-  res.status(200).json({message: 'Get tricks'})
+const getTricks = asyncHandler(async (req, res) => {
+  const tricks = await Trick.find()
+
+  res.status(200).json(tricks)
 })
 
 // @desc Get tricks
 // @route POST /api/tricks
 // @access Private
 
-const setTricks = aysncHandler(async (req, res) => {
+const setTrick = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new  Error('Please add a text field')
   }
 
-  res.status(200).json({message: 'Get tricks'})
+  const trick = await Trick.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(trick)
 })
 
 // @desc Get tricks
 // @route PUT /api/tricks
 // @access Private
 
-const updateTricks = aysncHandler(async (req, res) => {
-  res.status(200).json({message: `Update tricks ${req.params.id}`})
+const updateTrick = asyncHandler(async (req, res) => {
+  const trick = await Trick.findById(req.params.id)
+
+  if(!trick) {
+    res.status(400)
+    throw new Error('Trick not found')
+  }
+
+  const updatedTrick = await Trick.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedTrick)
 })
 
 // @desc Get tricks
 // @route DELETE /api/tricks
 // @access Private
 
-const deleteTricks = aysncHandler(async (req, res) => {
-  res.status(200).json({message: `Delete tricks ${req.params.id}`})
+const deleteTrick = asyncHandler(async (req, res) => {
+  const trick =  await Trick.findByIdAndDelete(req.params.id)
+
+  if(!trick)   {
+    res.status(400)
+    throw new Error('Trick not found')
+  }
+
+  res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
   getTricks,
-  setTricks,
-  updateTricks,
-  deleteTricks
+  setTrick,
+  updateTrick,
+  deleteTrick
 }
